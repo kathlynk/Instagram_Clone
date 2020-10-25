024ea28c713aa69450cc332cc67cb9e055141c67
anizhou <annie.zhou.611@gmail.com>
2020-10-24 22:38:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/instagram_clone/LoginActivity.java
@@ -0,0 +1,111 @@
+package com.example.instagram_clone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+// A login screen that offers login via username and passwords
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                checkEmpty(username, password);
+                loginUser(username, password);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick register button");
+                // Create the ParseUser
+                ParseUser user = new ParseUser();
+                // Set core properties
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                checkEmpty(username, password);
+                user.setUsername(username);
+                user.setPassword(password);
+                // Invoke signUpInBackground
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Log.i(TAG, "user properly registered!");
+                        } else {
+                            Log.e(TAG, "user unable to be registered",e);
+                        }
+                    }
+                });
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void checkEmpty(String username, String password) {
+        if (username.isEmpty()) {
+            Toast.makeText(LoginActivity.this, "Username cannot be empty", Toast.LENGTH_SHORT).show();
+        }
+        if (password.isEmpty()) {
+            Toast.makeText(LoginActivity.this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    // TODO: better error handling
+                    Log.e(TAG, "Issue with login", e);
+                    Toast.makeText(LoginActivity.this, "Issue with login!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // TODO: navigate to the main activity if the user has signed in properly
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
